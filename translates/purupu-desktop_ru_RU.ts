0bece6773fe98d670fa4eaff36a70f898b55afe2
tenglor <isdigni@gmail.com>
2023-01-22 11:04:13 +0300

    added Packing.cmake

--- translates/purupu-desktop_ru_RU.ts
+++ translates/purupu-desktop_ru_RU.ts
@@ -1,3 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU"></TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <location filename="../build/purupu-desktop_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
